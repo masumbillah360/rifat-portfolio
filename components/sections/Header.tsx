47520615888abf348ca431b75ce0606682cafa42
dev-billah <dev.billah@gmail.com>
2024-07-11 15:37:53 +0600

    added home pages banner section

--- components/sections/Header.tsx
+++ components/sections/Header.tsx
@@ -1,61 +1,64 @@
-'use client';
+"use client";
+
+import Image from "next/image";
+import { useGSAP } from "@gsap/react";
+import { animateWithGsap } from "@/lib/utils";
 
 const Header = () => {
+  useGSAP(() => {
+    animateWithGsap(
+      ".gText",
+      {
+        opacity: 1,
+        y: 0,
+        duration: 1,
+        ease: "power2.inOut",
+      },
+      {}
+    );
+    animateWithGsap(
+      ".gSubText",
+      { opacity: 1, y: 0, duration: 1, delay: 1, ease: "power3.inOut" },
+      {}
+    );
+    animateWithGsap(
+      ".gDiv",
+      {
+        opacity: 1,
+        y: 0,
+        duration: 2,
+        ease: "power1.in",
+        scale: 1,
+        rotate: 360,
+      },
+      {}
+    );
+  }, []);
   return (
-    <div className="relative flex flex-col-reverse py-16 lg:pt-0 lg:flex-col lg:pb-0">
-      <div className="inset-y-0 top-0 right-0 z-0 w-full px-4 mx-auto md:px-0 lg:pr-0 lg:mb-0 lg:mx-0 lg:w-7/12 lg:max-w-full lg:absolute xl:px-0">
-        <svg
-          className="absolute left-0 hidden h-full text-white transform -translate-x-1/2 lg:block"
-          viewBox="0 0 100 100"
-          fill="currentColor"
-          preserveAspectRatio="none slice"
-        >
-          <path d="M50 0H100L50 100H0L50 0Z" />
-        </svg>
-        <img
-          className="object-cover w-full h-56 rounded shadow-lg lg:rounded-none lg:shadow-none md:h-96 lg:h-full"
-          src="https://images.pexels.com/photos/3184291/pexels-photo-3184291.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=2&amp;h=750&amp;w=1260"
-          alt=""
-        />
-      </div>
-      <div className="relative flex flex-col items-start w-full px-4 mx-auto md:px-0 lg:px-8 lg:max-w-screen-xl">
-        <div className="mb-16 lg:my-40 lg:max-w-lg lg:pr-5">
-          <p className="inline-block px-3 py-px mb-4 text-xs font-semibold tracking-wider text-teal-900 uppercase rounded-full bg-teal-accent-400">
-            Brand new
-          </p>
-          <div className="mb-5 font-sans text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl sm:leading-none">
-            Everything you
-            <br className="hidden md:block" />
-            can imagine{" "}
-            <span className="inline-block text-deep-purple-accent-400">
-              is real
-            </span>
+    <div className="flex flex-col-reverse md:flex-row gap-8 md:gap-3 justify-end md:justify-between items-center min-h-screen">
+      <div className="size-full flex justify-center items-center">
+        <div className="size-full text-center flex flex-col gap-4">
+          <div className="headingText gText">SR GRAPHIC STUDIO</div>
+          <div className="underline underline-offset-8 opacity-0 transform translate-y-20 gSubText">
+            Graphic Design & Branding
           </div>
-          <p className="pr-5 mb-5 text-base text-gray-700 md:text-lg">
-            Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-            accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
-            quae. explicabo.
-          </p>
-          <div className="flex items-center">
-            <a
-              href="/"
-              className="inline-flex items-center justify-center h-12 px-6 mr-6 font-medium tracking-wide  transition duration-200 rounded shadow-md bg-deep-purple-accent-400 hover:bg-deep-purple-accent-700 focus:shadow-outline focus:outline-none"
-            >
-              Get started
-            </a>
-            <a
-              href="/"
-              aria-label=""
-              className="inline-flex items-center font-semibold text-gray-800 transition-colors duration-200 hover:text-deep-purple-accent-700"
-            >
-              Learn more
-            </a>
+        </div>
+      </div>
+      <div className="w-full h-1/2 md:h-full flex justify-center items-center mt-8 md:mt-0">
+        <div className="flex justify-center items-center">
+          <div className="size-72 p-2 rounded-full border border-rose-500 gDiv opacity-0 transform -translate-y-20 scale-0">
+            <Image
+              src="/assets/images/test-image.jpg"
+              alt="Test Image"
+              height={1000}
+              width={1000}
+              className="size-full rounded-full border"
+            />
           </div>
         </div>
       </div>
     </div>
   );
 };
-
 
 export default Header;
--- lib/utils.ts
+++ lib/utils.ts
@@ -1,5 +1,6 @@
+import gsap from "gsap";
 import { twMerge } from "tailwind-merge";
-import { UTApi } from "uploadthing/server";
+import { ScrollTrigger } from "gsap/all";
 import { type ClassValue, clsx } from "clsx";
 import type { OurFileRouter } from "@/app/api/uploadthing/core";
 import {
@@ -14,4 +15,20 @@ export function cn(...inputs: ClassValue[]) {
 export const UploadButton = generateUploadButton<OurFileRouter>();
 export const UploadDropzone = generateUploadDropzone<OurFileRouter>();
 
+gsap.registerPlugin(ScrollTrigger);
 
+export const animateWithGsap = (
+  target: string,
+  animationProps: gsap.TweenVars,
+  scrollProps: ScrollTrigger.Vars
+) => {
+  gsap.to(target, {
+    ...animationProps,
+    scrollTrigger: {
+      trigger: target,
+      toggleActions: "restart reverse restart reverse",
+      start: "top 85%",
+      ...scrollProps,
+    },
+  });
+};
